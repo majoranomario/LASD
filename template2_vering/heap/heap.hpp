5d747bc8a53afea8cbac30dd697abfa4ceaa3bd6
majoranomario <ma.majorano@studenti.unina.it>
2025-06-13 18:06:15 +0200

    Add files via upload

---
+++ template2_vering/heap/heap.hpp
@@ -0,0 +1,51 @@
+
+#ifndef HEAP_HPP
+#define HEAP_HPP
+
+/* ************************************************************************** */
+
+#include "../container/linear.hpp"
+
+/* ************************************************************************** */
+
+namespace lasd {
+
+/* ************************************************************************** */
+
+template <typename Data>
+class Heap {
+  // Must extend SortableLinearContainer<Data>,
+  //             ClearableContainer
+
+private:
+
+protected:
+
+public:
+
+  // Destructor
+  // ~Heap() specifiers
+
+  /* ************************************************************************ */
+
+  // Copy assignment
+  // type operator=(argument); // Copy assignment of abstract types is not possible.
+
+  // Move assignment
+  // type operator=(argument); // Move assignment of abstract types is not possible.
+
+  /* ************************************************************************ */
+
+  // Specific member functions
+
+  // type IsHeap(argument) specifiers;
+
+  // type Heapify(argument) specifiers;
+
+};
+
+/* ************************************************************************** */
+
+}
+
+#endif
